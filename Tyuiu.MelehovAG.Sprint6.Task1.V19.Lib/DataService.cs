a895e2e593605cd012d00026b24dbc4452e64625
Tyuiu.MelehovAG@outlook.com <dedmeleh@gmail.com>
2023-12-12 04:37:50 +0500

    Task1

--- Tyuiu.MelehovAG.Sprint6.Task1.V19.Lib/DataService.cs
+++ Tyuiu.MelehovAG.Sprint6.Task1.V19.Lib/DataService.cs
@@ -7,7 +7,30 @@ using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.MelehovAG.Sprint6.Task1.V19.Lib
 {
-    public class DataService
+    public class DataService : ISprint6Task1V19
     {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                try
+                {
+                    y = Math.Round(Math.Cos(x) + (Math.Cos(x) / (x + 2)) - (3 * x), 2);
+                    valueArray[count] = y;
+                }
+                catch (DivideByZeroException)
+                {
+                    valueArray[count] = 0;
+                }
+
+                count++;
+            }
+            return valueArray;
+        }
     }
 }
--- Tyuiu.MelehovAG.Sprint6.Task1.V19.Test/DataServiceTest.cs
+++ Tyuiu.MelehovAG.Sprint6.Task1.V19.Test/DataServiceTest.cs
@@ -10,6 +10,31 @@ namespace Tyuiu.MelehovAG.Sprint6.Task1.V19.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = (stopValue - startValue) + 1;
+
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+            valueWaitArray[0] = 15.19;
+            valueWaitArray[1] = 11.67;
+            valueWaitArray[2] = 9.00;
+            valueWaitArray[3] = double.NegativeInfinity;
+            valueWaitArray[4] = 4.08;
+            valueWaitArray[5] = 1.5;
+            valueWaitArray[6] = -2.28;
+            valueWaitArray[7] = -6.52;
+            valueWaitArray[8] = -10.19;
+            valueWaitArray[9] = -12.76;
+            valueWaitArray[10] = -14.68;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valueWaitArray, res);
         }
     }
 }
